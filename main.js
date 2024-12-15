aeba3ea6ee793157416a6da06350ecdf32146098
rajitchaudhary <rajitchaudhary34@gmail.com>
2024-12-15 13:44:03 +0530

    faq bug fixed

--- main.js
+++ main.js
@@ -40,13 +40,14 @@ document.querySelectorAll('section').forEach(section => {
 });
 
 document.querySelectorAll('.faq-question').forEach(button => {
-      button.addEventListener('click', () => {
-          button.classList.toggle('active');
-              const answer = button.nextElementSibling;
-                  if (answer.style.display === 'block') {
-                        answer.style.display = 'none';
-                            } else {
-                                  answer.style.display = 'block';
-                                      }
-                                        });
-                                        });
+     button.addEventListener('click', () => {
+     button.classList.toggle('active');
+        const answer = button.nextElementSibling;
+         if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+         }
+          else {
+          answer.style.display = 'block';
+         }
+          });
+  });
